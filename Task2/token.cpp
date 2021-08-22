47bc9fc07de1ebadc9dcee82d9b32614414ee0a2
bmitay4 <bmitay7@Gmail.com>
2021-08-22 10:27:28 +0300

    token edited

--- Task2/token.cpp
+++ Task2/token.cpp
@@ -1,8 +1,25 @@
 #include "token.h"
 
-#include "iostream"
-using namespace std;
+#include "tokenstrings.h"
 
+tokenType Token::getType() {
+    return this->type;
+}
+string Token::getText() {
+    return this->text;
+}
+//Note that shared_ptr is virtual
+shared_ptr<set<int>> varToken::getLines() {
+    return this->lines;
+}
+void varToken::add_line(int l) {
+    this->lines.get()->insert(l);
+}
+//If the word type value is less than 258, it will be printed as a character, otherwise the value name will be printed in enum
 void Token::printToken() {
-    cout << type << "\t" << text << endl;
+    if (type > 258) {
+        int enum_idx = type - 258;
+        cout << tokenStrings[enum_idx] << '\t' << text << endl;
+    } else
+        cout << (char)type << '\t' << text << endl;
 }
